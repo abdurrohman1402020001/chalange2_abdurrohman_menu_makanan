1f13ebd036dac535df3754aef5f31a72fd146c58
Abdurrohman11 <erna40408@gmail.com>
2023-09-10 17:58:22 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "chalange2_menu_makanan"
+include(":app")
+ 
